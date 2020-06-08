2530368dfe995bc85377296cb0c70f6fd3d5b2d8
Marcos Tischer Vallim <tischer@gmail.com>
2020-06-07 22:58:48 -0300

    initial version

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/mvallim/gluster-simple-csi-driver
 
 go 1.13
+
+require (
+	github.com/container-storage-interface/spec v1.3.0
+	github.com/kubernetes-csi/drivers v1.0.2
+	github.com/mvallim/gluster-csi-driver v0.0.0-20200608012251-822d0f40cc26 // indirect
+)
--- pkg/glusterfs/driver.go
+++ pkg/glusterfs/driver.go
@@ -10,26 +10,32 @@ const (
 	glusterfsCSIDriverVersion = "1.0.0"
 )
 
+// Driver
 type Driver struct {
 	*config.Config
 }
 
+// New
 func New(config *config.Config) (*Driver, error) {
 	return nil, nil
 }
 
+// NewControllerServer
 func NewControllerServer(dr *Driver) *ControllerServer {
 	return nil
 }
 
+// NewNodeServer
 func NewNodeServer(dr *Driver) *NodeServer {
 	return nil
 }
 
+// NewIdentityServer
 func NewIdentityServer(dr *Driver) *IdentityServer {
 	return nil
 }
 
+// Run
 func (dr *Driver) Run() {
 	srv := csicommon.NewNonBlockingGRPCServer()
 	srv.Start("g.Endpoint", NewIdentityServer(dr), NewControllerServer(dr), NewNodeServer(dr))
