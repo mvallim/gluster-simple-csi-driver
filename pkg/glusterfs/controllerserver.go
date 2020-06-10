f342679227cc1be6dd8ed84508d9e39f1fdd5419
Marcos Tischer Vallim <tischer@gmail.com>
2020-06-10 18:32:14 -0300

    initial version

--- pkg/glusterfs/controllerserver.go
+++ pkg/glusterfs/controllerserver.go
@@ -12,14 +12,15 @@ import (
 	"github.com/container-storage-interface/spec/lib/go/csi"
 )
 
-// ControllerServer
+// ControllerServer struct of GlusterFS CSI driver with supported methods of CSI controller server spec.
 type ControllerServer struct {
 	*Driver
 }
 
-// CreateVolume
+// CreateVolume creates and starts the volume
 func (cs *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
-	glog.V(2).Infof("request received %+v", protosanitizer.StripSecrets(req))
+
+	glog.V(2).Infof("received controller create volume request %+v", protosanitizer.StripSecrets(req))
 
 	if req == nil {
 		return nil, status.Errorf(codes.InvalidArgument, "request cannot be empty")
@@ -40,12 +41,10 @@ func (cs *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVol
 		}
 	}
 
-	glog.V(1).Infof("creating volume with name %s", req.Name)
-
 	return nil, nil
 }
 
-// DeleteVolume
+// DeleteVolume deletes the given volume.
 func (cs *ControllerServer) DeleteVolume(ctx context.Context, req *csi.DeleteVolumeRequest) (*csi.DeleteVolumeResponse, error) {
 	return nil, nil
 }
@@ -60,37 +59,37 @@ func (cs *ControllerServer) ControllerUnpublishVolume(ctx context.Context, req *
 	return nil, nil
 }
 
-// ValidateVolumeCapabilities
+// ValidateVolumeCapabilities checks whether the volume capabilities requested are supported.
 func (cs *ControllerServer) ValidateVolumeCapabilities(ctx context.Context, req *csi.ValidateVolumeCapabilitiesRequest) (*csi.ValidateVolumeCapabilitiesResponse, error) {
 	return nil, nil
 }
 
-// ListVolumes
+// ListVolumes returns a list of volumes
 func (cs *ControllerServer) ListVolumes(ctx context.Context, req *csi.ListVolumesRequest) (*csi.ListVolumesResponse, error) {
 	return nil, nil
 }
 
-// GetCapacity
+// GetCapacity returns the capacity of the storage pool
 func (cs *ControllerServer) GetCapacity(ctx context.Context, req *csi.GetCapacityRequest) (*csi.GetCapacityResponse, error) {
 	return nil, nil
 }
 
-// ControllerGetCapabilities
+// ControllerGetCapabilities returns the capabilities of the controller service.
 func (cs *ControllerServer) ControllerGetCapabilities(ctx context.Context, req *csi.ControllerGetCapabilitiesRequest) (*csi.ControllerGetCapabilitiesResponse, error) {
 	return nil, nil
 }
 
-// CreateSnapshot
+// CreateSnapshot create snapshot of an existing PV
 func (cs *ControllerServer) CreateSnapshot(ctx context.Context, req *csi.CreateSnapshotRequest) (*csi.CreateSnapshotResponse, error) {
 	return nil, nil
 }
 
-// DeleteSnapshot
+// DeleteSnapshot delete provided snapshot of a PV
 func (cs *ControllerServer) DeleteSnapshot(ctx context.Context, req *csi.DeleteSnapshotRequest) (*csi.DeleteSnapshotResponse, error) {
 	return nil, nil
 }
 
-// ListSnapshots
+// ListSnapshots list the snapshots of a PV
 func (cs *ControllerServer) ListSnapshots(ctx context.Context, req *csi.ListSnapshotsRequest) (*csi.ListSnapshotsResponse, error) {
 	return nil, nil
 }
