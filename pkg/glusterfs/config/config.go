f2a95f0410f6f68b72eb56a0e5bca04de6fb02fc
Marcos Tischer Vallim <tischer@gmail.com>
2020-06-10 18:43:49 -0300

    initial version

--- pkg/glusterfs/config/config.go
+++ pkg/glusterfs/config/config.go
@@ -1,10 +1,12 @@
 package config
 
+// Config struct fills the parameters of request or user input
 type Config struct {
 	Endpoint string
 	NodeID   string
 }
 
+//NewConfig returns config struct to initialize new driver
 func NewConfig() *Config {
 	return &Config{}
 }
